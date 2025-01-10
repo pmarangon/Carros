efa1f4e241804d5602ff7963ef51214e91215c88
unknown <paulammarangon@gmail.com>
2025-01-09 21:51:59 -0300

    Adiciona campos placa e photo na tabela car

--- app/urls.py
+++ app/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf import settings
+from django.conf.urls.static import static
 from cars.views import cars_view
 
 
@@ -25,4 +26,4 @@ from cars.views import cars_view
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cars/', cars_view),
-]
+] +static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
