acfc61c87f24d5d5f8de4428dd643c81437a8b3e
unknown <paulammarangon@gmail.com>
2025-01-12 20:18:25 -0300

    Exclui classe CarForm

--- cars/forms.py
+++ cars/forms.py
@@ -1,27 +1,6 @@
 from django import forms
 from cars.models import Brand, Car
 
-class Carform(forms.Form):
-    model = forms.CharField(max_length=200)
-    brand = forms.ModelChoiceField(queryset=Brand.objects.all())  # Corrigido para queryset
-    factory_year = forms.IntegerField()
-    model_year = forms.IntegerField()
-    plate = forms.CharField(max_length=10)
-    value = forms.FloatField()
-    photo = forms.ImageField()
-
-    def save(self):
-        car = Car(
-            model=self.cleaned_data['model'],
-            brand=self.cleaned_data['brand'],
-            factory_year=self.cleaned_data['factory_year'],
-            model_year=self.cleaned_data['model_year'],
-            plate=self.cleaned_data['plate'],
-            value=self.cleaned_data['value'],
-            photo=self.cleaned_data['photo'],
-        )
-        car.save()
-        return car
 
 
 
