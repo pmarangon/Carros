140ab56beb9c970bb4fc8b5fb6865e8006ddfa99
unknown <paulammarangon@gmail.com>
2025-01-12 18:42:05 -0300

    Adiciona  rota, view pra cadastro de carro

--- cars/views.py
+++ cars/views.py
@@ -1,20 +1,29 @@
 from django.shortcuts import render
 from cars.models import Car
-
+from cars.forms import Carform
 
 def cars_view(request):
     cars = Car.objects.all()
     search = request.GET.get('search')
     
     if search:
-        cars = Car.objects.filter(model__contains=search)
+        cars = Car.objects.filter(model__icontains=search).order_by('model')
    
     return render(
        request,
        'cars.html',
        {'cars': cars}
-       
-   )
+    )
+
+def new_car_view(request):  
+    if request.method =='POST':
+        pass
+    else:
+         new_car_form = Carform()  
+    return render(request, 'new_car.html', {'new_car_form': new_car_form})
+
+
+        
    
       
                    
