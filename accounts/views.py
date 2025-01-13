877d9cf5612d021ea1631c798ed2cb840ffb1e42
unknown <paulammarangon@gmail.com>
2025-01-12 21:29:13 -0300

    Cria app , url, view e template pra cadastro de usuarios

---
+++ accounts/views.py
@@ -0,0 +1,7 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render
+
+def register_view(request):
+    user_form = UserCreationForm()
+    return render(request, 'register.html', {'user_form': user_form})
+  
--- cars/forms.py
+++ cars/forms.py
@@ -1,9 +1,6 @@
 from django import forms
 from cars.models import Brand, Car
 
-
-
-
 class CarModelForm(forms.ModelForm):
     class Meta:
         model = Car
@@ -12,12 +9,11 @@ class CarModelForm(forms.ModelForm):
     def clean_value(self):
         value = self.cleaned_data.get('value')
         if value < 20000:
-           self.add_error('value', 'valor mínimo do carro  deve ser de R$ 20.000')
-           return value
+            self.add_error('value', 'O valor mínimo do carro deve ser de R$ 20.000')
+        return value
            
-      def clean_factory_year(self):
-         factory_year = self.cleaned_data.get('factory_year')
-         if factory_year <1975 :
+    def clean_factory_year(self):
+        factory_year = self.cleaned_data.get('factory_year')
+        if factory_year < 1975:
             self.add_error('factory_year', 'Não é possível cadastrar carros fabricados antes de 1975')
-            return factory_year
-            
+        return factory_year
